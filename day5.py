0baffb3221b8f69278e5e6426a38a54b8914f1c3
Divanshisharma02 <divanshisharma225115@gmail.com>
2025-06-23 20:32:03 +0530

    added day 5 python file

---
+++ day5.py
@@ -0,0 +1,50 @@
+#loops
+#python day5.py
+
+# for i in range (10):
+#     print("Hello")
+#*****************************************************************
+# i=1
+# while(i<=5):
+#     print(i)
+#     i=i+1
+# print("Done")
+
+#*******************************************************************
+# i=1
+# while(i<=11):
+#     print(i)
+#     i=i+2
+# print("Done")
+
+#*********************************************************************
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1,6,2):
+#     print(i)
+  
+# for i in range(1,11):
+#     print("5 * ",i,"=",5*i)
+
+# for i in range (1,20):
+#     if(i%2==0):
+#         print(i)
+        
+# for i in range (1,11):
+#     if(i%5==0):
+#         break
+#     print(i)
+
+# for i in range (1,11):
+#     if(i%2==0):
+#         continue
+#     print(i)
+
+# i=1
+# x=int (input("enter the no:"))
+# while(i<11):
+#     print("x * ",i,"=",x*i)
+#     i+=1
+
