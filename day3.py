c55120c8665f0ac03b18a063c7fefda0bf182f32
Divanshisharma02 <divanshisharma225115@gmail.com>
2025-06-23 20:26:06 +0530

    day3.py

---
+++ day3.py
@@ -0,0 +1,28 @@
+# name=input("Enter your name:")
+# print(name)
+# num1=int(input("enter the first number:"))
+# num2=int(input("enter the second number:"))
+# print(num1+num2)
+# print(type (num1),type (num2))
+
+# num1=input("enter the first number:")
+# num2=input("enter the second number:")
+# num3=int(num1)
+# num4=int(num2)
+# print(num1+num2)
+
+# age=int(input("enter your AGE:"))
+# print("next year age will be",age+1)
+
+#basic arathmatic operator
+#+,-,*,%(modulo),/,//Floor devesion
+# a=10
+# b=5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a//b)
+
+print((10+20)/5+5*4)
