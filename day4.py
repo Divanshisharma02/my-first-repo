49f70b63f94405568d69448b74552d4809472c5f
Divanshisharma02 <divanshisharma225115@gmail.com>
2025-06-23 20:31:25 +0530

    added day 4 python file

---
+++ day4.py
@@ -0,0 +1,81 @@
+# Conditational staterments 
+# Comparison operator /Relational operator (<,>,==,!=,<=,>=)
+
+
+#python day4.py
+
+# *******************************************************
+# a=10
+# b=20
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a<=b)
+# print(a>=b)
+
+# c=10
+# d=100
+# print(c!=d)
+
+            # if statemrnt ********************************************************
+
+# is_student =False
+# if (is_student == True):
+#     print ("yor are a student")
+    
+# print("yore are not a student")
+#**********************************************************************************
+# age=20
+# if (age>=18):
+#     print ("you can vote")
+# else:
+#     print("you cant't vote")
+
+          # if else statement ******************************************************
+
+# is_student =True
+# if (is_student == True):
+#     print ("yor are a student")
+# else:    
+#     print("yore are not a student")
+#*****************************************************
+# marks=60
+# if(marks>=60):
+#     print("you are pass")
+# else:
+#     print("you are fail")
+# *********************************
+# a=10
+# print(a%2)
+# print(a%2==0)
+# if(a%2==0):
+#     print("the number is even")
+# else:
+#     print("number is odd")
+
+             # if-elif-else statement *************************************************
+
+# age=11
+# if(age<=18):
+#     print("you can vote")
+# elif(age<=10):
+#     print("you are a kid")
+# else:
+#     ("you can't vote")
+    #grading question ***********************************************************
+# marks=50
+
+# if(marks>=90):
+#     print("A")
+# elif(marks>=80):
+#     print("B")
+# elif(marks>=70):
+#     print("C")
+# elif(marks>=60):
+#     print("D")
+# elif(marks<30):
+#     print("pd le bhai")
+# else:
+#     print("fail")
+
